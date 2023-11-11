45569081fa75786c1f8f04f2abbc2e2eb904fcb1
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-11-11 11:09:18 +0200

    Fix starting script to accept local connections

--- ethnode/start-node.sh
+++ ethnode/start-node.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name dev-geth -it -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp -v ~/.ethereum:/root/.ethereum ethereum/client-go:stable --datadir /root/.ethereum --dev --http --http.api eth,web3,net --http.corsdomain "http://remix.ethereum.org"
+docker run -it -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp --name dev-geth -v ~/.ethereum:/root/.ethereum ethereum/client-go:stable --datadir /root/.ethereum --dev --http --http.addr 0.0.0.0 --http.api eth,web3,net --http.corsdomain "http://remix.ethereum.org"
