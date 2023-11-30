4239dd0497417611ca34739ca3872fdbdbc0cb97
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-11-30 11:39:27 +0200

    format: add break lines

--- ethnode/start-node.sh
+++ ethnode/start-node.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-docker run -it -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp --name dev-geth -v ~/.ethereum:/root/.ethereum ethereum/client-go:stable --datadir /root/.ethereum --dev --http --http.addr 0.0.0.0 --http.api eth,web3,net --http.corsdomain "http://remix.ethereum.org"
+docker run -it -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp --name dev-geth -v ~/.ethereum:/root/.ethereum ethereum/client-go:stable \
+  --http.vhosts '*,localhost,host.docker.internal' \
+  --http \
+  --http.api eth,net,web3,debug \
+  --http.corsdomain '*' \
+  --http.addr "0.0.0.0" \
+  --nodiscover --maxpeers 0 --mine \
+  --networkid 1337 \
+  --dev \
+  --allow-insecure-unlock \
+  --rpc.allow-unprotected-txs \
+  --miner.gaslimit 12000000
